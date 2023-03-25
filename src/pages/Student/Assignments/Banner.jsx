7732c286e7265204dd96e6b71bf9a00ab1c00091
Leonardo Almeida <leonardoalmeida7@ua.pt>
2023-03-25 15:09:22 +0000

    MP-46 add assignments page

---
+++ src/pages/Student/Assignments/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Banner(){
+
+    return (
+        <>
+            <div data-aos='fade-up'>
+                <div className='font-bold text-5xl mb-4'>
+                    Assignments
+                </div>
+                <div className='text-xl mb-4 w-1/2'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    <p/>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                    blanditiis praesentium voluptatum deleniti atque corrupti quos
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Banner
