cfcd98aa49fd91f3251775284c40c30c34045e47
Leonardo Almeida <leonardoalmeida7@ua.pt>
2023-03-27 15:19:43 +0100

    remove scroll from navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -99,57 +99,62 @@ export default function MyNavbar(props){
 
     return (
         <>
-            <Navbar fluid={true} className='drop-shadow-md'>
-                <div>
-                <Navbar.Brand as={Link} to="/professor">
-                    <img
-                        src= {logo}
-                        className="mr-3 h-9 md:h-12 sm:h-9"
-                        alt="MepML Logo"
-                        />
-                        <span className="self-center whitespace-nowrap text-3xl font-extrabold logo-font">
-                            MepML
-                        </span>
-                </Navbar.Brand>
-                </div>
-                <div className="flex md:order-2">
-                    <div className='pl-10'/>
-                    <DarkThemeToggle />
-                    <div className="ml-4" />
-                    <Dropdown
-                    arrowIcon={false}
-                    inline={true}
-                    label={<Avatar
-                        placeholderInitials = {nameInitials()}
-                        rounded={true}
-                        status="online"
-                        statusPosition="bottom-right"
-                        />}
-                    >
-                    <Dropdown.Header>
-                        <span className="block text-sm">
-                        {session.user.name}
-                        </span>
-                        <span className="block truncate text-sm font-medium">
-                        {session.user.email}
-                        </span>
-                    </Dropdown.Header>
-                    <Dropdown.Item className="w-full" onClick={handleLogout}>
-                        <FaSignOutAlt className="mr-2 mt-1" />
-                        Sign out
-                    </Dropdown.Item>
-                    </Dropdown>
-                    <Navbar.Toggle />
-                </div>
-                <Navbar.Collapse>
-                    {session.type === 'professor' ? profLinks() : studentLinks()}
-                </Navbar.Collapse>
-            </Navbar>
+            <div className='h-screen flex flex-col'>
 
-            <Outlet />     
+                <Navbar fluid={true} className='drop-shadow-md'>
+                    <div>
+                    <Navbar.Brand as={Link} to="/professor">
+                        <img
+                            src= {logo}
+                            className="mr-3 h-9 md:h-12 sm:h-9"
+                            alt="MepML Logo"
+                            />
+                            <span className="self-center whitespace-nowrap text-3xl font-extrabold logo-font">
+                                MepML
+                            </span>
+                    </Navbar.Brand>
+                    </div>
+                    <div className="flex md:order-2">
+                        <div className='pl-10'/>
+                        <DarkThemeToggle />
+                        <div className="ml-4" />
+                        <Dropdown
+                        arrowIcon={false}
+                        inline={true}
+                        label={<Avatar
+                            placeholderInitials = {nameInitials()}
+                            rounded={true}
+                            status="online"
+                            statusPosition="bottom-right"
+                            />}
+                        >
+                        <Dropdown.Header>
+                            <span className="block text-sm">
+                            {session.user.name}
+                            </span>
+                            <span className="block truncate text-sm font-medium">
+                            {session.user.email}
+                            </span>
+                        </Dropdown.Header>
+                        <Dropdown.Item className="w-full" onClick={handleLogout}>
+                            <FaSignOutAlt className="mr-2 mt-1" />
+                            Sign out
+                        </Dropdown.Item>
+                        </Dropdown>
+                        <Navbar.Toggle />
+                    </div>
+                    <Navbar.Collapse>
+                        {session.type === 'professor' ? profLinks() : studentLinks()}
+                    </Navbar.Collapse>
+                </Navbar>
+                
+                <div className='flex-1 overflow-y-auto'>
+                    <Outlet />
+                </div>
 
-            {/* <Particles id="tsparticles" options={options} init={particlesInit} /> */}
-            {console.log(Particles, options, particlesInit)}
+                {/* <Particles id="tsparticles" options={options} init={particlesInit} /> */}
+                {console.log(Particles, options, particlesInit)}
+            </div>
         </>
     )
 }
